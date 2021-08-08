c3aa3dc034f25381b24f6b876f7ab9824c7a8716
Vladimir Vorobiev <vvs-it@bk.ru>
2021-08-08 17:53:39 +0600

    lesson06 ready

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const guessNumber = function () {
+  let randomNum = parseInt(Math.random() * 100);
+  let userNum = prompt('Угадай число от 1 до 100');
+  let attempts = 10;
+  attempts--;
+  function startGame() {
+    if (userNum == randomNum) {
+      let result = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      function restart() {
+        if (result === true) {
+          return guessNumber();
+        } else {
+          alert('Спасибо за игру');
+        }
+      }
+      restart();
+    } else if (userNum === null) {
+      alert('Игра окончена.');
+    } else if (userNum < randomNum && attempts > 0) {
+      userNum = prompt(`Загаданное число меньше, у вас осталось ${attempts} попыток`);
+      attempts--;
+      return startGame();
+    } else if (userNum > randomNum && attempts > 0) {
+      userNum = prompt(`Загаданное число больше, у вас осталось ${attempts} попыток`);
+      attempts--;
+      return startGame();
+    } else if (isNaN(userNum)) {
+      userNum = prompt('Введи число!');
+      startGame();
+    } else if (attempts === 0) {
+      let result = confirm('Попытки закончились. Хотите сыграть еще?');
+      function restart() {
+        if (result === true) {
+          return guessNumber();
+        } else {
+          alert('Спасибо за игру');
+        }
+      }
+      restart();
+    }
+  }
+  return startGame();
+};
+
+guessNumber();
